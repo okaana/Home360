4e7e5bcaa7885d7d205b8f2ece7f05ac7d466085
okaana <okaana9238@gmail.com>
2019-10-13 04:47:29 -0400

    Lab Assignment

---
+++ GeofenceLab3/app/src/main/java/bg/awesome/sc/geofencelab3/MapsActivity.java
@@ -0,0 +1,242 @@
+package bg.awesome.sc.geofencelab3;
+
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GeoQueryEventListener {
+
+    private GoogleMap mMap;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Marker currentUser;
+    private DatabaseReference myLocationRef;
+    private GeoFire geoFire;
+    private List<LatLng> Areas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+                        buildLocationRequest();
+                        buildLocationCallback();
+                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
+
+
+                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                                .findFragmentById(R.id.map);
+                        mapFragment.getMapAsync(MapsActivity.this);
+
+                        initArea();
+                        settingGeofire();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MapsActivity.this,"Permission Denied .... why?",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+
+
+
+    }
+
+    private void initArea() {
+        Areas = new ArrayList<>();
+        Areas.add(new LatLng(41.50,-81.60));
+        Areas.add(new LatLng(41.47,-81.55));
+    }
+
+    private void settingGeofire() {
+        myLocationRef = FirebaseDatabase.getInstance().getReference("MyLocation");
+        geoFire = new GeoFire(myLocationRef);
+
+    }
+
+    private void buildLocationCallback() {
+        locationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(final LocationResult locationResult) {
+                if(mMap != null){
+
+
+                    geoFire.setLocation("You", new GeoLocation(locationResult.getLastLocation().getLatitude()
+                            , locationResult.getLastLocation().getLongitude()), new GeoFire.CompletionListener() {
+                        @Override
+                        public void onComplete(String key, DatabaseError error) {
+                            if(currentUser != null) currentUser.remove();
+                            currentUser = mMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(locationResult.getLastLocation().getLatitude(),locationResult.getLastLocation().getLongitude()))
+                                    .title(locationResult.getLastLocation().getLatitude() + " , " + locationResult.getLastLocation().getLongitude()));
+
+                            //move camera
+                            mMap.animateCamera(CameraUpdateFactory
+                                    .newLatLngZoom(currentUser.getPosition(),12.0f));
+                        }
+                    });
+
+
+                }
+            }
+        };
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setSmallestDisplacement(10f);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        if (fusedLocationProviderClient != null)
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper());
+        //Add circles for Areas
+        for(LatLng latLng : Areas){
+            mMap.addCircle(new CircleOptions().center(latLng)
+            .radius(500)     // 500m
+            .strokeColor(Color.BLUE)
+            .fillColor(0x220000FF)
+            .strokeWidth(5.0f));
+
+            // create Geo Query  when area enterd
+            GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(latLng.latitude,latLng.longitude),0.5f);  // responds to 500m
+            geoQuery.addGeoQueryEventListener(MapsActivity.this);
+        }
+    }
+    @Override
+    protected void onStop() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        super.onStop();
+    }
+
+    @Override
+    public void onKeyEntered(String key, GeoLocation location) {
+        sendNotification("InLocation",String.format("%s entered the area",key));
+    }
+
+    @Override
+    public void onKeyExited(String key) {
+        sendNotification("InLocation",String.format("%s exited the area",key));
+    }
+
+    @Override
+    public void onKeyMoved(String key, GeoLocation location) {
+        sendNotification("InLocation",String.format("%s moved in the area",key));
+    }
+
+    @Override
+    public void onGeoQueryReady() {
+
+    }
+
+    @Override
+    public void onGeoQueryError(DatabaseError error) {
+        Toast.makeText(this,""+error.getMessage(),Toast.LENGTH_LONG).show();
+    }
+
+    private void sendNotification(String title, String content) {
+        String NOTIFICATOION_CHANNEL_ID = "our_area_multiple";
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATOION_CHANNEL_ID,"My Notification",NotificationManager.IMPORTANCE_DEFAULT);
+
+            //config
+            notificationChannel.setDescription("Channel Description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,NOTIFICATOION_CHANNEL_ID);
+        builder.setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(false)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+
+        Notification notification = builder.build();
+        notificationManager.notify(new Random().nextInt(),notification);
+    }
+}
